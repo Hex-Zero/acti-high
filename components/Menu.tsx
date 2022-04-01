db6876fa71fa6daf90b257ceddb0dab4ff5de088
Maxwell <hex0@live.com>
2022-04-01 22:17:40 +0100

    add menu layout

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -4,10 +4,19 @@ import style from "../styles/Menu.module.scss";
 export interface IMenuProps {}
 
 export function Menu(props: IMenuProps) {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <div className={style.menuWrapper}>
-      <div className={style.menuOverlay}></div>
-      <div className={style.menuContainer}></div>
+      <div
+        onClick={(e) => setIsOpen(!isOpen)}
+        className={style.menuOverlay}
+      ></div>
+      <div className={`${style.menuContainer} ${isOpen ? style.active : ""}`}>
+        <span
+          onClick={(e) => setIsOpen(!isOpen)}
+          className={style.closeButton}
+        />
+      </div>
     </div>
   );
 }
