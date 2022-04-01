c4adebc762f675b05aa4408c79f77ac531434952
Maxwell <hex0@live.com>
2022-04-01 02:58:19 +0100

    split components

--- components/ActivityCard.tsx
+++ components/ActivityCard.tsx
@@ -1,7 +1,50 @@
 import * as React from "react";
+import {
+  Draggable,
+  DraggingStyle,
+  NotDraggingStyle,
+} from "react-beautiful-dnd";
 
-export interface IActivityCardProps {}
+export interface IActivityCardProps {
+  item: any;
+  index: number;
+}
+
+const grid = 8;
+
+const getItemStyle = (
+  isDragging: boolean,
+  draggableStyle: DraggingStyle | NotDraggingStyle | undefined
+): any => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
 
 export function ActivityCard(props: IActivityCardProps) {
-  return <div>Active</div>;
+  const { item, index } = props;
+  return (
+    <Draggable key={item.id} draggableId={item.id} index={index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={getItemStyle(
+            snapshot.isDragging,
+            provided.draggableProps.style
+          )}
+        >
+          {item.content}
+        </div>
+      )}
+    </Draggable>
+  );
 }
--- components/DraggableContainer.tsx
+++ components/DraggableContainer.tsx
@@ -9,6 +9,7 @@ import {
   DraggingStyle,
   NotDraggingStyle,
 } from "react-beautiful-dnd";
+import { ActivityCard } from "./ActivityCard";
 
 interface IListItem {
   id: string;
@@ -31,27 +32,9 @@ const reorder = (list: IListItem[], startIndex: number, endIndex: number) => {
   return result;
 };
 
-const grid = 8;
-
-const getItemStyle = (
-  isDragging: boolean,
-  draggableStyle: DraggingStyle | NotDraggingStyle | undefined
-): any => ({
-  // some basic styles to make the items look a bit nicer
-  userSelect: "none",
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
-
-  // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
-
-  // styles we need to apply on draggables
-  ...draggableStyle,
-});
-
 const getListStyle = (isDraggingOver: Boolean) => ({
   background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: grid,
+  padding: 8,
   width: 250,
 });
 
@@ -86,21 +69,7 @@ export function DraggableContainer() {
               style={getListStyle(snapshot.isDraggingOver)}
             >
               {items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
+                <ActivityCard key={item.id} item={item} index={index} />
               ))}
               {provided.placeholder}
             </div>
